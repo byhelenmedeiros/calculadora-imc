ad05b625c804c30cfeed70d163c38dbc90972167
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-04-01 22:28:16 +0100

    alert

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
+function calcular() {
+    const peso = Number(document.querySelector("#peso").value);
+    const altura = Number(document.querySelector("#altura").value);
+    
+    const imc = peso / (altura * altura);
 
-function calcular(){
-
-}
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        alert("Por favor, preencha os campos com valores válidos.");
+        return;
+      }
+  
+    const res = document.querySelector(".res");
+    res.innerHTML = `Seu IMC é: ${imc.toFixed(2)}`;
+  }
